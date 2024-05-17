4f6585b1ddd0b85ceadda0c037f4244080ae4367
Renan Massini <renan.massini00@gmail.com>
2024-05-17 09:49:15 -0300

    create-login

--- project-angular-test/src/app/login/login.component.ts
+++ project-angular-test/src/app/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username === 'teste' && this.password === '123') {
+      this.router.navigateByUrl('/alunos');
+    } else {
+      this.errorMessage = 'Usuário ou senha inválidos';
+    }
+  }
 }
+
